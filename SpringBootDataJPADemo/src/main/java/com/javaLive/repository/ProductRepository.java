fe9dd5c35ba7a254acb46637c91578af54dfaa44
Administrator <Administrator@DELL>
2020-10-27 14:55:12 +0530

    Initial commit with project guide file.

---
+++ SpringBootDataJPADemo/src/main/java/com/javaLive/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.javaLive.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.javaLive.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
---
+++ SpringBootDataJPADemo/src/main/java/com/javaLive/service/ProductService.java
@@ -0,0 +1,36 @@
+package com.javaLive.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.javaLive.model.Product;
+import com.javaLive.repository.ProductRepository;
+
+@Service
+@Transactional
+@ConfigurationProperties(prefix="spring.datasource")
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repo;
+	
+	public List<Product> listAll() {
+		return repo.findAll();
+	}
+	
+	public void save(Product product) {
+		repo.save(product);
+	}
+	
+	public Product get(long id) {
+		return repo.findById(id).get();
+	}
+	
+	public void delete(long id) {
+		repo.deleteById(id);
+	}
+}
